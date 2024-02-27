5b42f05de4b1ca983463780bc9b3b6b1ddb51557
ROHIT KUMAR <kumar111222rohit@gmail.com>
2024-02-27 14:20:29 +0000

    added comments

--- services/character.service.ts
+++ services/character.service.ts
@@ -1,6 +1,12 @@
-import getURL from '../utils/rickAndMortyUrl';
-//generic APICall method
+import getURL from "../utils/rickAndMortyUrl";
 export default class Character {
+  /**
+   * Asynchronously fetches data from a given URL with specified request options.
+   *
+   * @param {string} URL - The endpoint URL.
+   * @param {object} requestOptions - Options for the fetch request.
+   * @returns {Promise<any>} JSON response data or throws an error on failure.
+   */
   static async makeAPICall(URL: string, requestOptions: object) {
     try {
       const response = await fetch(URL, requestOptions);
@@ -12,32 +18,40 @@ export default class Character {
     }
   }
 
-  //get all character data on initial load
-  static async fetchCharacterData(page: any) {
+  /**
+   * Fetches character data from a specified page.
+   *
+   * @param {any} page - The page number to fetch data from.
+   * @returns {Promise<any>} The character data from the API.
+   */ static async fetchCharacterData(page: any) {
     const requestOptions = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     };
     return Character.makeAPICall(
-      `${getURL('FETCH_CHARACTER_DATA')}?page=${page}`,
-      requestOptions
+      `${getURL("FETCH_CHARACTER_DATA")}?page=${page}`,
+      requestOptions,
     );
   }
-  //get episodes data array
+  /**
+   * Fetches data for multiple episodes in a single call to avoid pagination.
+   *
+   * @returns {Promise<any>} The episode data from the API.
+   */
   static async fetchEpisodeData() {
     const requestOptions = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     };
     //passing all episodes as parameter to get the entire result in one call instead of paginated result.
     return Character.makeAPICall(
-      `${getURL('FETCH_EPISODE_DATA')}` +
-        '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41',
-      requestOptions
+      `${getURL("FETCH_EPISODE_DATA")}` +
+        "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41",
+      requestOptions,
     );
   }
 }
