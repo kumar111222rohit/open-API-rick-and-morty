d242684a92af36d0f0a3679fa85eb660efa6d043
ROHIT KUMAR <kumar111222rohit@gmail.com>
2024-02-27 17:32:36 +0000

    refactor config

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,6 @@
 import '@testing-library/jest-dom';
 jest.mock('next/config', () => () => ({
-    publicRuntimeConfig: {
-      // Your runtime config values here
-      localeSubpaths: {},
-      // Any other publicRuntimeConfig values your app expects
-    },
-  }));
-  
+  publicRuntimeConfig: {
+    localeSubpaths: {},
+  },
+}));
